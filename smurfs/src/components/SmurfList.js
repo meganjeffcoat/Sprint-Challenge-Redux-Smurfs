daa586ace85f04253b7936edd57acff499d28d5f
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-01 13:04:14 -0600

    papa smurf img added

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Smurf from './Smurf';
+import papasmurf from './papasmurf.png'
+
 
 const SmurfList = props => {
     return(
         <div className='smurf-container'>
             <h1>Smurf List</h1>
-            <img src='papasmurf.png' alt='papasmurf' />
+            <img src={papasmurf} alt='papasmurf' />
             <div className='smurf-list'>
             {props.smurfs.map((s, i) => <Smurf smurf={s} key={i}/>)}
             </div>
