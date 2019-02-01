60f1d47398977a06843c6bbbaf1bd7fbaac5ac8e
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-01 10:45:58 -0600

    set up file structure for actions and reducers in appropriate js files

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,13 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  START_REQUEST,
+  REQUEST_SUCCESS,
+  REQUEST_FAILED,
+  ADD_SMURF,
+  GET_SMURF
+} from '../actions';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,6 +20,26 @@
    error: null
  }
 */
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false
+  addingSmurf: false
+  updatingSmurf: false
+  deletingSmurf: false
+  error: null
+}
+
+const reducer = (stat, action) => {
+  switch(action.type){
+  
+    
+  default:
+    return state;  
+  }
+}
+
+export default reducer:
 
 /*
   You'll only need one smurf reducer for this project.
